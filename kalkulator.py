e4c3bbd4bce216f9251ae4fb95d06e5a57ac6d87
Sławomir Jagniątkowski <50059@student.ue.poznan.pl>
2017-04-11 19:49:52 +0200

    rozwiazalem konflikt

--- kalkulator.py
+++ kalkulator.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-def dodawanie():
-	a = int(input('Podaj pierwsza liczbe'))
-	b = int(input('Podaj druga liczbe'))
-	print(a+b)
-
 def get_help():
 	print('To jest prosty program kalkulatora. Wprowadz 2 liczby i zatwierdz enterem.')
 
-get_help()
-dodawanie()
-=======
 def dodawanie(a, b):
 	wynik = a + b
 	return wynik
 	
+get_help()
 zm1 = int(input())
 zm2 = int(input())
 print(dodawanie(zm1, zm2))
->>>>>>> zmiana_funkcji_dodawania
