b7f0cc4ed13bd54ee14b77175c88b94af8ff1018
Sławomir Jagniątkowski <50059@student.ue.poznan.pl>
2017-04-11 19:25:16 +0200

    dodano funkcje pomoc

--- kalkulator.py
+++ kalkulator.py
@@ -2,5 +2,9 @@ def dodawanie():
 	a = int(input('Podaj pierwsza liczbe'))
 	b = int(input('Podaj druga liczbe'))
 	print(a+b)
-	
+
+def get_help():
+	print('To jest prosty program kalkulatora. Wprowadz 2 liczby i zatwierdz enterem.')
+
+get_help()
 dodawanie()
